b939e9809226174366ef78667254f76258a4f5e6
pryelluw <pryelluw@gmail.com>
2020-08-05 18:24:39 -0400

    cleanup

--- bin/postMessage.js
+++ bin/postMessage.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const slack = require("../lib/slack/index.js");
+const slack = require("../lib/slack/slack.js");
 
 const text = "Heroku Scheduler ran";
 slack.send_message(process.env.SLACK_WEBHOOK_URL, text);
-
